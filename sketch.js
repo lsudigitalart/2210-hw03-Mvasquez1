99881acd4ecbc91879fb8bb4139e9a3fe2d7ce69
michael <mvasq12@lsu.edu>
2024-09-21 19:01:10 -0600

    push3

---
+++ sketch.js
@@ -0,0 +1,28 @@
+function setup()
+{
+    createCanvas(1000,1000);
+    background(100);
+
+}
+
+function draw()
+{
+    var i = 0;
+    for (var i = 5; i < width +10000; i += 10)
+    {
+      for (var j = 5; j < height; j += 10)
+      {
+        if (i < 10000)
+        {
+          //fill(random(225)); //greyscale
+          fill(color(random(255), random(255), random(255))); //color
+          ellipse(i, j, 111, 10);
+        } else
+        end;
+        //continue;
+        }
+     } 
+    }
+//ctrl/shift/p 
+//clone --paste link to git
+//make new file
