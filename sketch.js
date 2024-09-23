424a217bf2ebe051036c0e90236a79fa9cd74de4
michael <mvasq12@lsu.edu>
2024-09-23 11:15:35 -0600

    push3

--- sketch.js
+++ sketch.js
@@ -7,22 +7,22 @@ function setup()
 
 function draw()
 {
-    var i = 0;
-    for (var i = 5; i < width +10000; i += 10)
+  var i = 0;
+  for (var i = 5; i < width +10000; i += 10)
+  {
+    for (var j = 5; j < height; j += 10)
     {
-      for (var j = 5; j < height; j += 10)
-      {
-        if (i < 10000)
-        {
-          //fill(random(225)); //greyscale
-          fill(color(random(255), random(255), random(255))); //color
-          ellipse(i, j, 111, 10);
-        } else
-        end;
-        //continue;
-        }
-     } 
+    if (i < 10000)
+    {
+      //fill(random(225)); //greyscale
+      fill(color(random(255), random(255), random(255))); //color
+      ellipse(i, j, 111, 10);
+    } else
+    end;
+    //continue;
     }
+    } 
+  }
 //ctrl/shift/p 
 //clone --paste link to git
 //make new file
